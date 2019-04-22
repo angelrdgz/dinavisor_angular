a3d3fb55c1e3ef39124b603dae0448071a46d9a9
Angel <angelrodriguez@ucol.mx>
2019-04-22 16:13:27 -0500

    all things

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
 import { SiteLayoutComponent } from './_layouts/site-layout/site-layout.component';
 import { SiteHeaderComponent } from './_layouts/site-header/site-header.component';
 import { SiteFooterComponent } from './_layouts/site-footer/site-footer.component';
 import { HomeComponent } from './home/home.component';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
@@ -21,7 +29,12 @@ import { FlexLayoutModule } from '@angular/flex-layout';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    SlickCarouselModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,114 @@
 import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import {MatSnackBar} from '@angular/material';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+	public slides:any;
+	public data:boolean = false;
+	public contact:any = {name:'', last:'', phone:'', comment:''};
+	durationInSeconds = 5;
 
-  ngOnInit() {
-  }
+	constructor(
+		private http: HttpClient,
+		private snackBar: MatSnackBar
+	) { }
+
+	ngOnInit() {
+
+		this.http.get("http://127.0.0.1:8000/api/get-clients").subscribe(
+        (val) => {
+        	console.log(val)
+        	this.slides = val;
+        	this.data = true;
+            //console.log("PATCH call successful value returned in body", val);
+        },
+        response => {
+            console.log("PATCH call in error", response);
+        },
+        () => {
+            console.log("The PATCH observable is now completed.");
+        });
+	}
+
+	openSnackBar(message: string, action: string) {
+	    this.snackBar.open(message, action, {
+	      duration: 4000,
+	    });
+	  }
+
+	slideConfig = {
+		"slidesToShow": 4, 
+		"slidesToScroll": 2,
+		"autoplay": true,
+		"autoplaySpeed": 5000,
+		"responsive": [
+		{
+			"breakpoint": 1024,
+			"settings": {
+				"slidesToShow": 3,
+				"slidesToScroll": 3,
+				"infinite": true,
+				"dots": true
+			}
+		},
+		{
+			"breakpoint": 600,
+			"settings": {
+				"slidesToShow": 2,
+				"slidesToScroll": 2
+			}
+		},
+		{
+			"breakpoint": 480,
+			"settings": {
+				"slidesToShow": 1,
+				"slidesToScroll": 1,
+				"autoplaySpeed": 2000,
+			}
+		}]
+	};
+
+	
+
+	slickInit(e) {
+		console.log('slick initialized');
+	}
+
+	breakpoint(e) {
+		console.log('breakpoint');
+	}
+
+	afterChange(e) {
+		console.log('afterChange');
+	}
+
+	beforeChange(e) {
+		console.log('beforeChange');
+	}
+
+	sendEmail(){
+		console.log(this.contact)
+		this.http.post("http://127.0.0.1:8000/api/test-email", this.contact).subscribe(
+        (val) => {
+        	console.log(val)
+        	this.openSnackBar('Email enviadó con éxito', 'OK');     	
+        },
+        response => {
+            console.log("PATCH call in error", response);
+            this.openSnackBar('Error al enviar su petición', 'OK');
+        },
+        () => {
+            console.log("The PATCH observable is now completed.");
+            this.openSnackBar('Email enviadó con éxito', 'OK');
+        });
+	}
 
 }
